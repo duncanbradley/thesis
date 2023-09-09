67662fe1e76ae49f3ae13d52cdd3960a4fca3daa
Duncan Bradley <47359202+duncanbradley@users.noreply.github.com>
2023-09-09 18:58:02 +0100

    solved density without trailing tails

--- Chapters/axis-extension/testing-raincloud.R
+++ Chapters/axis-extension/testing-raincloud.R
@@ -6,28 +6,32 @@ e2 %>%
   ggplot(aes(x = mag_slider.response, 
              y = axis)) +
   geom_density_ridges(scale = 0,
-                      colour = "green", 
-                      alpha = 1,
+                      colour = "lightgrey",
+                      alpha = 0,
                       jittered_points = T,
-                      position = position_raincloud(height = 0.2),
+                      position = position_raincloud(height = 0.15),
                       point_alpha = 0.1,
-                      point_colour = "grey"
-  ) +
-  geom_density(scale = 0.2, colour = "red") +
+                      point_colour = "darkgrey",
+                      point_size = 0.5) +
+  geom_density_ridges(aes(height = after_stat(density)),
+                      stat = "density",
+                      scale = 0.4,
+                      colour = "black",
+                      fill = "darkgrey") +
   geom_boxplot(outlier.shape=NA,
              width = 0.10,
              colour = "white",
              fill = "white",
              alpha = 0,
              lwd = 1,
-             position = position_nudge(y=-.15)) +
+             position = position_nudge(y=-.18)) +
   geom_boxplot(outlier.shape=NA,
                width = 0.09,
                colour = "black",
                fill = "white",
                alpha = 0.7,
                lwd = 0.5,
-               position = position_nudge(y=-.15)) +
+               position = position_nudge(y=-.18)) +
   scale_y_discrete(limits = c("extend", "default"),
                    labels = c("Extended\nAxis Limit", "Default\nAxis Limit")) + 
   labs(title = "Experiment 2 - Magnitude Ratings",
@@ -40,3 +44,45 @@ e2 %>%
   facet_wrap(vars(denominator), ncol = 1, labeller = labeller(denominator = denom_labs)) +
   theme_minimal(base_size = 16) +
   theme(aspect.ratio = 0.6)
+
+e1 %>%
+  ggplot(aes(x = slider.response, 
+             y = cond)) +
+  geom_density_ridges(scale = 0,
+                      colour = "lightgrey",
+                      alpha = 0,
+                      jittered_points = T,
+                      position = position_raincloud(height = 0.15),
+                      point_alpha = 0.1,
+                      point_colour = "darkgrey") +
+  geom_density_ridges(aes(height = after_stat(density)),
+                      stat = "density",
+                      scale = 0.4,
+                      colour = "black",
+                      fill = "darkgrey") +
+  geom_boxplot(outlier.shape=NA,
+               width = 0.10,
+               colour = "white",
+               fill = "white",
+               alpha = 0,
+               lwd = 1,
+               position = position_nudge(y=-.18)) +
+  geom_boxplot(outlier.shape=NA,
+               width = 0.09,
+               colour = "black",
+               fill = "white",
+               alpha = 0.7,
+               lwd = 0.5,
+               position = position_nudge(y=-.18)) +
+  scale_y_discrete(limits = c("extend", "default"),
+                   labels = c("Extended\nAxis Limit", "Default\nAxis Limit")) + 
+  labs(title = "Experiment 2 - Magnitude Ratings",
+       subtitle =  "Raw Data",
+       y = NULL,
+       x = NULL) + 
+  scale_x_continuous(labels = c('Very low\nmagnitude', 'Very high\nmagnitude'),
+                     breaks = c(1,2),
+                     minor_breaks = c()) + 
+  theme_minimal(base_size = 16) +
+  theme(aspect.ratio = 0.6)
+
